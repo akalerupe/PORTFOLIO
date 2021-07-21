d6f1b9017a3db212e4f685e3eb0890ff9c39439e
akal-bytes <akalerupe@gmail.com>
2021-07-21 20:49:40 +0300

    portfolio project

--- scripts.js
+++ scripts.js
@@ -1,13 +1,9 @@
-function openmenu(){
-    document.querySelector(".nav-links").style.display = "block";
-  }
-  function closemenu(){
-    document.querySelector(".nav-links").style.display = "none";
-  }
-  window.onresize = function(){
-      if(window.innerWidth > 600){
-        document.querySelector(".nav-links").style.display = "block";
-      }else{
-        document.querySelector(".nav-links").style.display = "none";
-      }
-  }
+const clickC = document.querySelector(".desc");
+
+clickC.addEventListener ("click", function (){
+  const r = Math.round(Math.random() * 255 + 0);
+  const g = Math.round(Math.random() * 255 + 0);
+  const b = Math.round(Math.random() * 255 + 0);
+  
+  document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
+});
